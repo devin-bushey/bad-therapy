8b07c49255c9c1510bafac8ead4f411a90c9263f
Devin Bushey <devin.m.bushey@gmail.com>
2025-05-01 20:33:42 -0700

    Init swift project

---
+++ BadTherapy/BadTherapyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BadTherapyApp.swift
+//  BadTherapy
+//
+//  Created by Devin Bushey on 2025-05-01.
+//
+
+import SwiftUI
+
+@main
+struct BadTherapyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BadTherapyTests/BadTherapyTests.swift
@@ -0,0 +1,17 @@
+//
+//  BadTherapyTests.swift
+//  BadTherapyTests
+//
+//  Created by Devin Bushey on 2025-05-01.
+//
+
+import Testing
+@testable import BadTherapy
+
+struct BadTherapyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
